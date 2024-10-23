664458ef6c375d194234e65d4f29cf9d768852fd
Jiulius Cesar Pereira Balog Silva <jiulius.balog.dev@gmail.com>
2024-10-23 12:03:01 -0300

    Prj_EstSeq09.java

--- Prj_EstSeq09.java
+++ Prj_EstSeq09.java
@@ -3,28 +3,20 @@
 // Data: 23/06/24
 // Objetivo do programa: Receba os 2 números inteiros. Calcule e mostre a soma dos quadrados.
 
-package Prj_EstSeq; // Nome do Pacote.
-import java.util.Scanner; // Biblioteca utilizada (Scanner).
+package Prj_EstSeq; 
+import javax.swing.JOptionPane; 
 
-public class Prj_EstSeq09 // Nome da Classe.
+public class Prj_EstSeq09 
 {
-    public static void main (String [] args) // Programa Principal.
+    public static void main (String [] args)
     {
-        try(Scanner input = new Scanner(System.in)) // Iniciaçização do Scanner.
-        {
-            int a; // Variável 1.
-            int b; // Variável 2.
-            int result; // Resultado.
-            
-            System.out.print("Insira o primeiro valor: "); // Imprime tal frase na tela do usuário.
-            a = input.nextInt(); // Coleta o valor da Variável 1.
-            System.out.print("Insira o segundo valor: "); // Imprime tal frase na tela do usuário.
-            b = input.nextInt(); // Coleta o valor da Variável 2.
-            
-            result = (a * 2) + (b * 2); // Cálculo da soma dos quadrados.
-            
-            System.out.println("Soma dos quadrados: " + result); // Exibe o resultado.
-            
-        }
+        int a, b, result;
+        
+        a = Integer.parseInt(JOptionPane.showInputDialog("Insira o primeiro valor: "));
+        b = Integer.parseInt(JOptionPane.showInputDialog("Insira o segundo valor: "));
+        
+        result = (a * 2) + (b * 2);
+        
+        JOptionPane.showMessageDialog(null, "Soma dos Quadrados: " + result);
     }
 }

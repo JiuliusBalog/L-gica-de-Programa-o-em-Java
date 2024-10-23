6f45d93e2e6825858f53a20aca7f8be998e81e35
Jiulius Cesar Pereira Balog Silva <jiulius.balog.dev@gmail.com>
2024-10-23 11:25:04 -0300

    Prj_EstSeq03.java

--- Prj_EstSeq03.java
+++ Prj_EstSeq03.java
@@ -3,27 +3,20 @@
 // Data: 23/06/24
 // Objetivo do programa: Receba a base e a altura de um triângulo. Calcule e mostre a sua área.
 
-package Prj_EstSeq; // Nome do pacote.
-import java.util.Scanner; // Biblioteca a ser utilizada (nesse caso, o Scanner para coleta de dados do usuário).
+package Prj_EstSeq;
+import javax.swing.JOptionPane;
 
-public class Prj_EstSeq03 // Nome da Classe.
+public class Prj_EstSeq03 
 {
-    public static void main (String [] args) // Início do Programa Principal.
+    public static void main (String [] args) 
     {
-        try(Scanner input = new Scanner(System.in)) // Inicialização do Scanner.
-        {
-            float base; // Variável 1.
-            float altura; // Variável 2.
-            float area; // Resultado do cálculo.
-                  
-            System.out.print("Insira a base do triângulo: "); // Imprime tal frase para o usuário.
-            base = input.nextFloat(); // Solicita envio de dados para a variável 1.
-            System.out.print("Insira a altura do triângulo: "); // Imprime tal frase para o usuário.
-            altura = input.nextFloat(); // Solicita envio de dados para a variáveç 2.
-            
-            area = (base * altura) / 2; // Cálculo do triângulo.
-            
-            System.out.println(area); // Imprime o resultado para o usuário.
-        }
+        double base, altura, area;
+        
+        base = Double.parseDouble(JOptionPane.showInputDialog("Insira a base do triângulo: "));
+        altura = Double.parseDouble(JOptionPane.showInputDialog("Insira a altura do triângulo: "));
+        
+        area = (base * altura) / 2;
+        
+        JOptionPane.showMessageDialog(null, "Área do triângulo: " + area);
     }
 }

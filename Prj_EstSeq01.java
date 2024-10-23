6f240d2b82d56225dc070c46b8898a47d1e04eaa
Jiulius Cesar Pereira Balog Silva <jiulius.balog.dev@gmail.com>
2024-10-23 11:20:02 -0300

    Prj_EstSeq01.java

--- Prj_EstSeq01.java
+++ Prj_EstSeq01.java
@@ -3,24 +3,18 @@
 // Data: 23/06/24
 // Objetivo do programa: Coletar o valor do lado de um quadrado, calcular sua área e apresentar o resultado.
 
-package Prj_EstSeq; // Pacote (ou a "pasta") do projeto
-import java.util.Scanner; // Biblioteca do Scanner
+package Prj_EstSeq;
+import javax.swing.JOptionPane;
 
-public class Prj_EstSeq01 // Classe
+public class Prj_EstSeq01
 {
-    public static void main (String [] args) // Programa principal
+    public static void main (String [] args)
     {
-        try(Scanner input = new Scanner(System.in)) // Inicia o Scanner
-        {
-            int lado; // Variável 1
-            int area; // Variável 2
-            
-            System.out.print("Insira o lado do quadrado: "); // Escreva ("Insira o lado do quadrado: ")
-            lado = input.nextInt(); // Leia (lado)
-            
-            area = lado * lado; // Cálculo
-            
-            System.out.println(area); // Escreva(area)
-        }
+        int lado, area;
+        
+        lado = Integer.parseInt(JOptionPane.showInputDialog("Insira o lado do quadrado: "));
+        
+        area = lado * lado;
+        JOptionPane.showMessageDialog(null, "Área do Quadrado: " + area);
     }
 }

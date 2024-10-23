ae33143490608b28f9fb46f2e16f3308cd6fb6f9
Jiulius Cesar Pereira Balog Silva <jiulius.balog.dev@gmail.com>
2024-10-23 12:12:21 -0300

    Prj_EstSeq17.java

--- Prj_EstSeq17.java
+++ Prj_EstSeq17.java
@@ -4,27 +4,20 @@
 // Objetivo do programa: Receba o valor de um depósito em poupança. Calcule e mostre o valor após 1 mês de aplicação sabendo que rende 1,3% a. m.
 
 package Prj_EstSeq;
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Prj_EstSeq17
 {
     public static void main (String [] args)
     {
-        try(Scanner input = new Scanner(System.in))
-        {    
-            int tempo;
-            double vmedia;
-            double litros;
+        int tempo;
+        double vmedia, litros;
+         
+        tempo = Integer.parseInt(JOptionPane.showInputDialog("Insira o tempo de percurso: "));
+        vmedia = Double.parseDouble(JOptionPane.showInputDialog("Insira a velocidade média: "));;
             
-            System.out.print("Insira o tempo: ");
-            tempo = input.nextInt();
-            System.out.print("Insira a velocidade média: ");
-            vmedia = input.nextInt();
+        litros = (tempo * vmedia) / 12;
             
-            litros = (tempo * vmedia) / 12;
-            
-            System.out.println("Quantidade de litros: " + litros);
-        }
-       
+        JOptionPane.showMessageDialog(null, "Quantidade de litros: " + litros);
     }
 }

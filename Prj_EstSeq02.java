54f396cefa9931ed245ec2a222e5f4a3ea95b4ce
Jiulius Cesar Pereira Balog Silva <jiulius.balog.dev@gmail.com>
2024-10-23 11:21:49 -0300

    Prj_EstSeq02.java

--- Prj_EstSeq02.java
+++ Prj_EstSeq02.java
@@ -3,24 +3,18 @@
 // Data: 23/06/24
 // Objetivo do programa: Receba o salário de um funcionário e mostre o novo salário com reajuste de 15%.
 
-package Prj_EstSeq; // Nome do pacote 
-import java.util.Scanner; // Nome da biblioteca usada (Scanner para coleta de valores do usuário)
+package Prj_EstSeq;
+import javax.swing.JOptionPane;
 
 public class Prj_EstSeq02 // Classe do projeto
 {
     public static void main (String [] args) // Programa principal
     {
-        try(Scanner input = new Scanner(System.in)) // Início do Scanner
-        {
-            int salario; // Variável 1
-            int salreajuste; // Variável 2
-            
-            System.out.print("Insira o seu salário: "); // Imprime a frase na tela
-            salario = input.nextInt(); // Solicita inserção de valor ao usuário
-            
-            salreajuste = (salario * 15) / 100 + salario;
-                    
-            System.out.println(salreajuste);
-        }
+        double salario, salarioliq;
+        
+        salario = Double.parseDouble(JOptionPane.showInputDialog("Insira o salário: "));
+        
+        salarioliq = (salario * 0.15) + salario;
+        JOptionPane.showMessageDialog(null, "Salário reajustado: " + salarioliq);
     }
 }

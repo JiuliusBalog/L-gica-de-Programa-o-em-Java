383afbb761b5005e524a74b2611a6dbac2b90030
Jiulius Cesar Pereira Balog Silva <jiulius.balog.dev@gmail.com>
2024-10-23 11:27:38 -0300

    Prj_EstSeq04.java

--- Prj_EstSeq04.java
+++ Prj_EstSeq04.java
@@ -3,25 +3,19 @@
 // Data: 23/06/24
 // Objetivo do programa: Receba a temperatura em graus Celsius. Calcule e mostre a sua temperatura convertida em fahrenheit F = (9*C+160) /5.
 
-package prj_estseq01; // Nome do Pacote.
-import java.util.Scanner; // Biblioteca utilizada (Scanner para coleta de dados do usuário).
+package Prj_EstSeq; 
+import java.util.Scanner; 
 
-public class Prj_EstSeq04 // Nome da Classe.
+public class Prj_EstSeq04 
 {
-    public static void main(String [] args) // Programa Principal.
+    public static void main(String [] args) 
     {
-        try(Scanner input = new Scanner(System.in)) // Inicialização do Scanner.
-        {
-            
-        int celsius; // Variável 1.
-        int fahrenheit; // Variável 2 como resultado do cálculo.
+        int celsius, fahrenheit;
         
-        System.out.print("Insira a temperatura em graus Celsius: "); // Imprime tal frase na tela para o usuário.
-        celsius = input.nextInt(); // Solicita coleta de dados para a variável 1.
+        celsius = Integer.parseInt(JOptionPane.showInputDialog("Insira a temperatura em graus Celsius: "));
         
-        fahrenheit = (9*celsius+160)/5; // Cálculo de conversão de Celsius para Fahrentheit
+        fahrenheit = (9 * celsius + 160) / 5;
         
-        System.out.println(fahrenheit); // Exibe o resultado pro usuário.
-        }
+        JOptionPane.showMessageDialog(null, "Temperatura em Fahrenheit: " + fahrenheit);
     }
 }

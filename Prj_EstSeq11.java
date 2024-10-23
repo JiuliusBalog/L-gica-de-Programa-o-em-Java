dd24ddb38c89701aa11f49a500303df472f28cc8
Jiulius Cesar Pereira Balog Silva <jiulius.balog.dev@gmail.com>
2024-10-23 12:05:40 -0300

    Prj_EstSeq11.java

--- Prj_EstSeq11.java
+++ Prj_EstSeq11.java
@@ -4,23 +4,18 @@
 // Objetivo do programa: Receba o valor de um depósito em poupança. Calcule e mostre o valor após 1 mês de aplicação sabendo que rende 1,3% a. m.
 
 package Prj_EstSeq;
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Prj_EstSeq11 
 {
     public static void main(String [] args)
     {
-        try(Scanner input = new Scanner(System.in))
-        {
-            double raio;
-            double circunferencia;
-            
-            System.out.print("Insira o raio da circunferência: ");
-            raio = input.nextDouble();
-            
-            circunferencia = (raio * 2) * 3.14;
-            
-            System.out.println("Valor de circunferência: " + circunferencia);
-        }
+        double raio, circunferencia;
+        
+        raio = Double.parseDouble(JOptionPane.showInputDialog("Insira o raio: "));
+        
+        circunferencia = (raio * 2) * 3.14;
+        
+        JOptionPane.showMessageDialog(null, "Circunferência: " + circunferencia);
     }
 }
